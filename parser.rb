66fbad142fcef27eefec683078f26f9dc519c18a
rabGIT <rabGIT@users.noreply.github.com>
2017-04-21 08:51:08 -0700

    added printing w/ attributes or html only

--- parser.rb
+++ parser.rb
@@ -2,6 +2,7 @@ require_relative 'tag'
 
 TagNode = Struct.new(:type, :attributes, :parent, :children)
 
+# DOM parser --> HTML in, produces tree with clean print
 class Parser
   def initialize(html_str)
     @buffer = html_str
@@ -15,6 +16,7 @@ class Parser
     tokenize
     parse_to_tree
     print_tree(@root)
+    print_html(@root)
   end
 
   def tokenize
@@ -31,34 +33,34 @@ class Parser
     @tokenized.each do |e|
       if e.match(/^<[\/].+>/)
         node = node.parent
-        node.children << TagNode.new(e, nil, node, [])
+        node.children << TagNode.new(e, Tag.new((e.match(/<(.+?)>/).captures[0]), nil, nil, nil, nil, nil), node, [])
       elsif e.match(/<.+>/)
         node.children << TagNode.new(e, parse_tag(e), node, [])
         node = node.children[-1]
       else
-        node.children << TagNode.new(e, nil, node, [])
+        node.children << TagNode.new(e, Tag.new('text', nil, nil, nil, nil, nil), node, [])
       end
     end
   end
 
-  def print_tree(node, indent = 0)
+  def print_tree(node, indent = 0, html_only = false)
     indent += 3
     print '   '.rjust(indent)
-    puts "#{node.type} #{node.attributes}"
+    puts "#{node.type.ljust(40 - indent)}#{node.attributes unless html_only}"
     return if node.children.nil?
     node.children.each do |n|
-      print_tree(n, indent)
+      print_tree(n, indent, html_only)
     end
   end
 
-
+  def print_html(node)
+    print_tree(node, 0, true)
+  end
 end
-
-
 
 # test code
 if __FILE__ == $PROGRAM_NAME
-  p = Parser.new("<div>  div text before  <p>    p text  </p>  <div>    more div text  </div>  div text after</div>")
+  p = Parser.new("< !doctype html><div>  div text before  <p>    p text  </p>  <div>    more div text  </div>  div text after</div>")
   p.parse
   p1 = Parser.new("<p>Before text <span>mid text (not included in text attribute of the paragraph tag)</span> after text</p>")
   p1.parse
