d7b917dbdfbc263a0a47fd98def727ff0a52e6c2
rabGIT <rabGIT@users.noreply.github.com>
2017-04-21 09:20:28 -0700

    added file handling for input

--- parser.rb
+++ parser.rb
@@ -4,12 +4,10 @@ TagNode = Struct.new(:type, :attributes, :parent, :children)
 
 # DOM parser --> HTML in, produces tree with clean print
 class Parser
-  def initialize(html_str)
-    @buffer = html_str
+  def initialize(filename)
+    @buffer = load_file(filename)
     @tokenized = []
     @root = TagNode.new('document', nil, nil, [])
-    @tag = /^(<.+?>)(.+)/
-    @text = /(.+?)(<.+)/
   end
 
   def parse
@@ -19,15 +17,49 @@ class Parser
     print_html(@root)
   end
 
+  def print_tree(node, indent = 0, html_only = false)
+    indent += 3
+    print '   '.rjust(indent)
+    puts "#{node.type.ljust(40 - indent)}#{node.attributes unless html_only}"
+    return if node.children.nil?
+    node.children.each do |n|
+      print_tree(n, indent, html_only)
+    end
+  end
+
+  def print_html(node)
+    print_tree(node, 0, true)
+  end
+
+  private
+
+  # if the file doesn't exist, assume the string is a html input and proceed
+  # otherwise, read the file into an array, string \n from each line and then
+  # put it back togeter as a string
+  def load_file(filename)
+    return @buffer = filename unless File.exist?(filename)
+    @buffer = File.readlines(filename)
+    @buffer.each_with_index do |line, index|
+      @buffer[index] = line.chomp
+    end
+    @buffer = @buffer.join
+  end
+
+  # load @tokenized as an array with each html element as a separate array
+  # element including text as a token
   def tokenize
+    re_tag = /^(<.+?>)(.+)/
+    re_text = /(.+?)(<.+)/
     until @buffer.empty?
-      parse = @buffer.match(@tag) ? @buffer.match(@tag) : @buffer.match(@text)
+      parse = @buffer.match(re_tag) ? @buffer.match(re_tag) : @buffer.match(re_text)
       return @tokenized << @buffer if parse.nil?
       @tokenized << parse.captures[0]
       @buffer = parse.captures[1].strip
     end
   end
 
+  # take the tokenized array and transalte into a tree structure using regex
+  # to match on token types to put them at the right level in the DOM tree
   def parse_to_tree
     node = @root
     @tokenized.each do |e|
@@ -44,26 +76,12 @@ class Parser
       end
     end
   end
-
-  def print_tree(node, indent = 0, html_only = false)
-    indent += 3
-    print '   '.rjust(indent)
-    puts "#{node.type.ljust(40 - indent)}#{node.attributes unless html_only}"
-    return if node.children.nil?
-    node.children.each do |n|
-      print_tree(n, indent, html_only)
-    end
-  end
-
-  def print_html(node)
-    print_tree(node, 0, true)
-  end
 end
 
 # test code
 if __FILE__ == $PROGRAM_NAME
   p = Parser.new("< !doctype html><div>  div text before  <p>    p text  </p>  <div>    more div text  </div>  div text after</div>")
   p.parse
-  p1 = Parser.new("<html><p>Before text <img src='bla'><span>mid text (not included in text attribute of the paragraph tag)</span> after text</p>")
+  p1 = Parser.new("test.html")
   p1.parse
 end
